0100366b4f7534f6790fc481f1e9954d09b4c1cb
Jinang Shah <jinang.shah@solutionanalysts.com>
2024-06-27 16:52:28 +0530

    updated package.json & added/updated react library.

--- src/components/TableReact.tsx
+++ src/components/TableReact.tsx
@@ -1,5 +1,5 @@
 import { ColumnDef, ColumnFiltersState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table';
-import React from 'react'
+import * as React from "react";
 import DebounceInput from './DebounceInput'
 
 type dynamicTableProp ={
