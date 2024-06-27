bf2799d05c2715d86422aa79b1b10cd645aafc7b
Jinang Shah <jinang.shah@solutionanalysts.com>
2024-06-27 17:02:42 +0530

    update in export functionality.

--- src/components/TableReact.tsx
+++ src/components/TableReact.tsx
@@ -1,51 +1,58 @@
-import { ColumnDef, ColumnFiltersState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table';
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 import * as React from "react";
-import DebounceInput from './DebounceInput'
+import DebounceInput from "./DebounceInput";
 
-type dynamicTableProp ={
-    data: []|[Object]|any,
-    columns: ColumnDef<any>[],
-} ;
+type dynamicTableProp = {
+  data: [] | [Object] | any;
+  columns: ColumnDef<any>[];
+};
 
-function TableReact(tableProp:dynamicTableProp) {
-    const { data,columns } = tableProp;
-    console.log(data,columns, 'data,columns');
-    
-    const [defaultColumns, setDefaultColumns ] = React.useState([...columns]);
-    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-        []
-      )
-    const table = useReactTable({
-        data,
-        columns: defaultColumns,
-        filterFns: {},
-        state: {
-          columnFilters,
-        },
-        onColumnFiltersChange: setColumnFilters,
-        getCoreRowModel: getCoreRowModel(),
-        getFilteredRowModel: getFilteredRowModel(), //client side filtering
-        getSortedRowModel: getSortedRowModel(),
-        getPaginationRowModel: getPaginationRowModel(),
-        manualPagination: true,
-        pageCount: Math.ceil(200 / 10),
-      });
+function TableReact(tableProp: dynamicTableProp) {
+  const { data, columns } = tableProp;
+  console.log(data, columns, "data,columns");
+
+  const [defaultColumns, setDefaultColumns] = React.useState([...columns]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const table = useReactTable({
+    data,
+    columns: defaultColumns,
+    filterFns: {},
+    state: {
+      columnFilters,
+    },
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(), //client side filtering
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    manualPagination: true,
+    pageCount: Math.ceil(200 / 10),
+  });
   return (
     <table className="table table-striped table-hover table-bordered">
-    <thead>
-      {table.getHeaderGroups().map((headerGroup) => (
-        <tr key={headerGroup.id}>
-          {headerGroup.headers.map((header) => (
-            <th key={header.id} colSpan={header.colSpan}>
-              {header.isPlaceholder
-                ? null
-                :  (
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id} colSpan={header.colSpan}>
+                {header.isPlaceholder ? null : (
                   <>
                     <div
                       {...{
                         className: header.column.getCanSort()
-                          ? 'cursor-pointer select-none'
-                          : '',
+                          ? "cursor-pointer select-none"
+                          : "",
                         onClick: header.column.getToggleSortingHandler(),
                       }}
                     >
@@ -54,45 +61,43 @@ function TableReact(tableProp:dynamicTableProp) {
                         header.getContext()
                       )}
                       {{
-                        asc: ' 🔼',
-                        desc: ' 🔽',
+                        asc: " 🔼",
+                        desc: " 🔽",
                       }[header.column.getIsSorted() as string] ?? null}
                     </div>
                     {header.column.getCanFilter() ? (
-                      <DebounceInput column={header.column}/>
+                      <DebounceInput column={header.column} />
                     ) : null}
                   </>
-                )
-              }
-            </th>
-          ))}
-        </tr>
-      ))}
-    </thead>
-    <tbody>
-      {table.getRowModel().rows.map((row) => (
-        <tr key={row.id}>
-          {row.getVisibleCells().map((cell) => (
-            <td key={cell.id}>
-              {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+        {table.getRowModel().rows.length === 0 && (
+          <tr>
+            <td
+              colSpan={table.getAllLeafColumns().length}
+              className="text-center"
+            >
+              Sorry! No Data Found!
             </td>
-          ))}
-        </tr>
-      ))}
-      {table.getRowModel().rows.length === 0 && (
-        <tr>
-          <td
-            colSpan={table.getAllLeafColumns().length}
-            className="text-center"
-          >
-            Sorry! No Data Found!
-          </td>
-        </tr>
-      )}
-    </tbody>
-   
-  </table>
-  )
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
 }
 
-export default TableReact
+export { TableReact };
